63b9c7663325e2061782536696364370728927de
Angelo Marasa <lpangm03@me.com>
2016-05-06 09:21:05 -0400

    First commit

---
+++ js/app.js
@@ -0,0 +1,15 @@
+$(function(){
+    $("#type").typed({
+        strings: [
+            "RB, this is not CSS!",
+            "Jquery, specifically a library called 'typed.js'",
+            "CSS can also do it, but with only one line.",
+            "You were close."
+        ],
+        typeSpeed: 30,
+        backDelay: 500,
+        loop: true,
+        showCursor: true,
+        cursorChar: "|",
+    });
+});
